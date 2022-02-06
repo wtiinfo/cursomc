dd112be9cc789ea54442af45adab54cf0fc562bb
wtiinfo <wanbrwan@gmail.com>
2022-02-06 13:57:30 -0200

    Instanciando durante a execucao

--- src/main/java/com/wtiinfo/cursomc/CursomcApplication.java
+++ src/main/java/com/wtiinfo/cursomc/CursomcApplication.java
@@ -1,13 +1,30 @@
 package com.wtiinfo.cursomc;
 
+import com.wtiinfo.cursomc.domain.Categoria;
+import com.wtiinfo.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Vendas");
+
+		categoriaRepo.saveAll(Arrays.asList(cat1, cat2, cat3));
+	}
 }
