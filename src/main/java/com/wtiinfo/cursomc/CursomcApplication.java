6abb506ad42faa400fb26b8c9dd692ce772ca77a
wtiinfo <wanbrwan@gmail.com>
2022-02-07 13:29:31 -0200

    Excessoes

--- src/main/java/com/wtiinfo/cursomc/CursomcApplication.java
+++ src/main/java/com/wtiinfo/cursomc/CursomcApplication.java
@@ -1,15 +1,19 @@
 package com.wtiinfo.cursomc;
 
 import com.wtiinfo.cursomc.domain.Categoria;
+import com.wtiinfo.cursomc.domain.Cidade;
+import com.wtiinfo.cursomc.domain.Estado;
 import com.wtiinfo.cursomc.domain.Produto;
 import com.wtiinfo.cursomc.repositories.CategoriaRepository;
+import com.wtiinfo.cursomc.repositories.CidadeRepository;
+import com.wtiinfo.cursomc.repositories.EstadoRepository;
 import com.wtiinfo.cursomc.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
@@ -19,6 +23,12 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private ProdutoRepository produtoRepo;
+
+	@Autowired
+	private EstadoRepository estadoRepo;
+
+	@Autowired
+	private CidadeRepository cidadeRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -33,14 +43,29 @@ public class CursomcApplication implements CommandLineRunner {
 		Produto p2 = new Produto(null, "Impressora", 890.00);
 		Produto p3 = new Produto(null, "Mouse", 78.90);
 
-		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
-		cat2.getProdutos().addAll(Arrays.asList(p2));
+		cat1.getProdutos().addAll(List.of(p1, p2, p3));
+		cat2.getProdutos().addAll(List.of(p2));
 
-		p1.getCategorias().addAll(Arrays.asList(cat1));
-		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
-		p3.getCategorias().addAll(Arrays.asList(cat1));
+		p1.getCategorias().addAll(List.of(cat1));
+		p2.getCategorias().addAll(List.of(cat1, cat2));
+		p3.getCategorias().addAll(List.of(cat1));
 
-		categoriaRepo.saveAll(Arrays.asList(cat1, cat2));
-		produtoRepo.saveAll(Arrays.asList(p1, p2, p3));
+		categoriaRepo.saveAll(List.of(cat1, cat2));
+		produtoRepo.saveAll(List.of(p1, p2, p3));
+
+		Estado e1 = new Estado(null, "Minas Gerais");
+		Estado e2 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "Uberlândia", e1);
+		Cidade c2 = new Cidade(null, "São Paulo", e2);
+		Cidade c3 = new Cidade(null, "Campinas", e2);
+
+		e1.getCidades().addAll(List.of(c1));
+		e2.getCidades().addAll(List.of(c2, c3));
+
+		estadoRepo.saveAll(List.of(e1, e2));
+		cidadeRepo.saveAll(List.of(c1, c2, c3));
+
 	}
+
 }
---
+++ src/main/java/com/wtiinfo/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,9 @@
+package com.wtiinfo.cursomc.repositories;
+
+import com.wtiinfo.cursomc.domain.Cidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+}
