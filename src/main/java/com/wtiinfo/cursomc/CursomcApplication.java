6d09fa3c394d8f79c53b8a1ef6edec11c5bc7902
wtiinfo <wanbrwan@gmail.com>
2022-02-07 14:28:50 -0200

    Cliente, TipoCliente, Telefone, Endereco

--- src/main/java/com/wtiinfo/cursomc/CursomcApplication.java
+++ src/main/java/com/wtiinfo/cursomc/CursomcApplication.java
@@ -1,13 +1,8 @@
 package com.wtiinfo.cursomc;
 
-import com.wtiinfo.cursomc.domain.Categoria;
-import com.wtiinfo.cursomc.domain.Cidade;
-import com.wtiinfo.cursomc.domain.Estado;
-import com.wtiinfo.cursomc.domain.Produto;
-import com.wtiinfo.cursomc.repositories.CategoriaRepository;
-import com.wtiinfo.cursomc.repositories.CidadeRepository;
-import com.wtiinfo.cursomc.repositories.EstadoRepository;
-import com.wtiinfo.cursomc.repositories.ProdutoRepository;
+import com.wtiinfo.cursomc.domain.*;
+import com.wtiinfo.cursomc.domain.enums.TipoCliente;
+import com.wtiinfo.cursomc.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -29,6 +24,12 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CidadeRepository cidadeRepo;
+
+	@Autowired
+	private EnderecoRepository enderecoRepo;
+
+	@Autowired
+	private ClienteRepository clienteRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -65,6 +66,16 @@ public class CursomcApplication implements CommandLineRunner {
 
 		estadoRepo.saveAll(List.of(e1, e2));
 		cidadeRepo.saveAll(List.of(c1, c2, c3));
+
+		Cliente cl1 = new Cliente(null, "Maria", "mari@gmail.com", "222.222.222-22", TipoCliente.PESSOA_FISICA);
+		cl1.getTelefones().addAll(List.of("4444-22222", "9999-1111"));
+
+		Endereco ed1 = new Endereco(null, "Rua Flores", "123", "Jardins", "Santa Luz", "09444-000", cl1, c1);
+		Endereco ed2 = new Endereco(null, "Rua Domingos", "321", "Estrada Velha", "Luzitano", "09444-320", cl1, c2);
+
+		cl1.getEnderecos().addAll(List.of(ed1, ed2));
+		clienteRepo.saveAll(List.of(cl1));
+		enderecoRepo.saveAll(List.of(ed1, ed2));
 
 	}
 
---
+++ src/main/java/com/wtiinfo/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.wtiinfo.cursomc.repositories;
+
+import com.wtiinfo.cursomc.domain.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+}
